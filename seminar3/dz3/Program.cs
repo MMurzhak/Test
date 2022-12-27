5884eaa9794a83508e9e1b38028e438cc6748d94
Mikhail <murzhakmd@mail.ru>
2022-12-27 16:48:42 +0300

    dz3

---
+++ seminar3/dz3/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.Clear();
+int n;
+while(true)
+{
+Console.Write("Введите число N: ");
+try
+    {
+        n = Convert.ToInt32(Console.ReadLine());
+        break;
+    }
+    catch(Exception exc)
+    {
+       Console.WriteLine($"Ошибка ввода! {exc.Message}"); 
+    }
+}
+for(int i = 1; i <= n ; i++)
+{
+    Console.Write($"{i * i * i}\t");
+}
