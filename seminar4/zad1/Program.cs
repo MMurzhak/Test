4e65667993b9d7e3bab525362577407228f80997
Mikhail <murzhakmd@mail.ru>
2023-01-14 13:05:19 +0300

    zad1

---
+++ seminar4/zad1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход число А и выдает сумму чисел от 1 до A
+//4->10 (1+2+3+4=10)
+//7->28
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число А: ","Ошибка Ввода!");
+int sumNumbers = GetSumNumbers(num);
+Console.WriteLine($"{num} -> {sumNumbers}");
+
+//Выводит в консоль сообщение message,
+//преобразовывыет введеную пользователем строку в число типа int.
+//в случае ошибки выводит в консоль сообщение errorMessage
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+//возвращаем сумму чисел от 1 до number
+
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum +=number;
+        number--;
+    }
+    return sum;
+}
