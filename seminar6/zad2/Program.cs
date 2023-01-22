bd74692c33a672d47bba7002389d12dd974854c7
Mikhail <murzhakmd@mail.ru>
2023-01-22 19:44:19 +0300

    zad

---
+++ seminar6/zad2/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Напишите программу, которая принимает на вход три числа и проверяет, может ли
+существовать треугольник с сторонами так длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+*/
+
+Console.Clear();
+
+int[] array = new int[3];
+
+FillArray(array);
+bool result = CheckArray(array);
+Console.WriteLine($"Треугольник со сторонами заданной длины существует: {(result ? "да" : "нет")}");
+
+void FillArray(int[] collection)
+{
+    int lengthArray = collection.Length;
+    for(int i = 0; i < lengthArray; i++)
+    {
+        while(true)
+        {
+            Console.Write($"Введите {i}-й элемент массива: ");
+            bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+            if(isCorrect)
+            {
+                collection[i] = num;
+                break;
+            }
+            else Console.WriteLine("Ошибка ввода! ");
+        }
+    }
+}
+
+bool CheckArray(int[] arr)
+{
+    Console.WriteLine(String.Join(" ", arr));
+    int size = arr.Length;
+    if(((arr[0] + arr[1]) > arr[2]) && ((arr[1] + arr[2]) > arr[0]) && ((arr[2] + arr[0]) > arr[1])) return true;
+    else return false;
+}
---
+++ seminar6/zad3/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45->101101
+3->11
+2->10
+*/
+
+Console.Clear();
+Console.WriteLine($" -> {Decimal()}");
+
+int GetNumberFromUser()
+{
+     
+        while(true)
+        {
+            Console.Write($"Введите число: ");
+            bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+            if(isCorrect)
+            {
+                return num;
+               
+            }
+            else Console.WriteLine("Ошибка ввода! ");
+        }
+    
+}
+
+string Decimal()
+{
+    int num = GetNumberFromUser();
+    string result = String.Empty;
+    if(num == 0) return "0";
+    while(num > 0)
+    {
+        result =(num % 2).ToString() + result;
+        num = num / 2;
+    }
+    return result;
+}
