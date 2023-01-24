d451289f25cbbb9d0700dccc97ec3a63f59781fc
Mikhail <murzhakmd@mail.ru>
2023-01-24 17:13:56 +0300

    dz2

---
+++ seminar6/dz1/sem6dz1.cs
@@ -0,0 +1,59 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.Clear();
+
+int[] array = new int[GetNumberFromUser()];
+
+FillArray(array);
+int result = CheckPositiveArray(array);
+Console.WriteLine($"[{String.Join(",", array)}]");
+Console.WriteLine($"Чисел больше 0: {result}");
+
+int GetNumberFromUser()
+{
+     
+        while(true)
+        {
+            Console.Write($"Введите число: ");
+            bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+            if(isCorrect)
+            {
+                return num;
+               
+            }
+            else Console.WriteLine("Ошибка ввода! ");
+        }
+    
+}
+
+void FillArray(int[] collection)
+{
+    int lengthArray = collection.Length;
+    for(int i = 0; i < lengthArray; i++)
+    {
+        while(true)
+        {
+            Console.Write($"Введите {i}-й элемент массива: ");
+            bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+            if(isCorrect)
+            {
+                collection[i] = num;
+                break;
+            }
+            else Console.WriteLine("Ошибка ввода! ");
+        }
+    }
+}
+
+int CheckPositiveArray(int[] arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0) count ++;
+    }
+    return count;
+}
+
---
+++ seminar6/dz2/sem6dz2.cs
@@ -0,0 +1,44 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.   x=(b2-b1)/(k1-k2);
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+Console.Clear();
+
+double[] array = new double[4];
+FillArray(array);
+
+double xy1 = CheckXY(array);
+double xy2 = CheckXY(array);
+Console.Write($"[{String.Join(",", array)}] ->");
+
+Console.Write($"({String.Join(",", xy1)})");
+Console.Write($"({String.Join(",", xy2)})");
+
+
+void FillArray(double[] collection)
+{
+    int lengthArray = collection.Length;
+    for(int i = 0; i < lengthArray; i++)
+    {
+        while(true)
+        {
+            Console.Write($"Введите {i+1}-ю координату точки: ");
+            bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+            if(isCorrect)
+            {
+                collection[i] = num;
+                break;
+            }
+            else Console.WriteLine("Ошибка ввода! ");
+        }
+    }
+}
+
+double CheckXY(double[] arr)
+{
+    double x = (arr[2]-arr[0])/(arr[1]-arr[3]);   
+    double y = arr[1] * x + arr[0];                           
+    return y;   
+}
+
